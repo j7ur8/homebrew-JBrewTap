6fc9e4a3f5b92b9163d5ddbf33fb79def63811c4
j7ur8 <j7ur8@users.noreply.github.com>
2026-03-18 12:08:25 +0800

    Add detect-it-easy cask

---
+++ Casks/detect-it-easy.rb
@@ -0,0 +1,14 @@
+cask "detect-it-easy" do
+  version "3.10"
+  desc "Program for determining types of files for Windows, Linux and MacOS."
+  homepage "https://github.com/horsicq/DIE-engine"
+  sha256 :no_check
+  url "https://github.com/horsicq/DIE-engine/releases/download/3.10/die_mac_3.10_x86_64.pkg"
+
+  pkg "die_mac_3.10_x86_64.pkg"
+
+  livecheck do
+    url "https://github.com/horsicq/DIE-engine/releases/latest"
+    strategy :github_latest
+  end
+end
