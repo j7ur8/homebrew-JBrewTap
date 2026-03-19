7d2e45735c278421d4ccb6ce2d7c3f72ce38abf3
j7ur8 <j7ur8@users.noreply.github.com>
2026-03-19 11:21:21 +0800

    Add gotohttp cask

--- Casks/gotohttp.rb
+++ Casks/gotohttp.rb
@@ -8,11 +8,29 @@ cask "gotohttp" do
   homepage "https://gotohttp.com/"
 
   installer manual: "Install.pkg"
+  uninstall early_script: {
+    executable:   "/usr/bin/xattr",
+    args:         ["-d", "com.apple.quarantine", "#{staged_path}/Uninstall.pkg"],
+    sudo:         false,
+    must_succeed: false,
+  }
   uninstall script: {
     executable: "/usr/bin/open",
     args:       ["#{staged_path}/Uninstall.pkg"],
     sudo:       false,
   }
+
+  caveats do
+    <<~EOS
+      If macOS blocks Install.pkg as from an unidentified developer,
+      Control-click Install.pkg, choose Open, then click Open again.
+
+      Or install this cask with:
+        brew install --cask --no-quarantine gotohttp
+
+      brew uninstall --cask gotohttp will open Uninstall.pkg for removal.
+    EOS
+  end
 
   livecheck do
     skip "The vendor provides an unversioned DMG with no stable version metadata."
