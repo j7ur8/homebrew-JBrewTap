25c772ad570afcc3b0633f3771715be0e50b937d
j7ur8 <j7ur8@users.noreply.github.com>
2026-03-19 10:37:38 +0800

    Add gotohttp cask

--- Casks/gotohttp.rb
+++ Casks/gotohttp.rb
@@ -1,18 +1,15 @@
 cask "gotohttp" do
   version :latest
-  sha256 :no_check
+  sha256 "88bcb9658730522dcbeaec5652576a8c050387b2e678ebd63dc2a927bec5faaf"
 
   url "https://gotohttp.com/gotohttp.dmg"
   name "GoToHTTP"
   desc "Remote access and control software"
   homepage "https://gotohttp.com/"
 
-  installer manual: "Install"
-  uninstall script: {
-    executable: "/usr/bin/open",
-    args:       ["#{staged_path}/Uninstall"],
-    sudo:       false,
-  }
+  pkg "Install.pkg"
+
+  uninstall pkgutil: "com.pingbo.gotohttp.Install"
 
   livecheck do
     skip "The vendor provides an unversioned DMG with no stable version metadata."
